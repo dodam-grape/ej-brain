ab2039b1d91c99d5d5247ad6d74841e145f9eb24
ej-project <eunjung5429@gmail.com>
2026-07-28 08:53:30 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+/* PC·모바일 동기화를 켤 때 Firebase 콘솔에서 받은 값을 입력하세요. */
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
